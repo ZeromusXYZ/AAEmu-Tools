78171e494bb7ab447c1845a1f4c8d3063ab6394f
ZeromusXYZ <zeromus.xyz@gmail.com>
2019-06-19 12:26:59 +0200

    -wip: passgen

---
+++ AAEmu.PassGen/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace AAEmu.PassGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AAEmu.PasGen");
+            Console.WriteLine("-------------");
+            Console.WriteLine();
+            Console.WriteLine("This tool can be used to help generate a hashed user password for your MySQL database");
+            Console.WriteLine();
+            Console.Write("Password: ");
+            var pass = Console.ReadLine();
+            SHA256Managed sha = new SHA256Managed();
+            sha.TransformBlock()
+        }
+    }
+}
