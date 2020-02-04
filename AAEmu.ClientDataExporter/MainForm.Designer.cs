f646b065237655bdc8c67b593ee6366e9e985175
ZeromusXYZ <zeromus.xyz@gmail.com>
2020-02-04 12:26:43 +0100

    -wip: add data exporter

---
+++ AAEmu.ClientDataExporter/MainForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace AAEmu.ClientDataExporter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LClientLocation = new System.Windows.Forms.Label();
+            this.btnFindClient = new System.Windows.Forms.Button();
+            this.clientFolderDlg = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnQuestSphere = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.exportFileDlg = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // LClientLocation
+            // 
+            this.LClientLocation.AutoSize = true;
+            this.LClientLocation.Location = new System.Drawing.Point(156, 17);
+            this.LClientLocation.Name = "LClientLocation";
+            this.LClientLocation.Size = new System.Drawing.Size(102, 13);
+            this.LClientLocation.TabIndex = 0;
+            this.LClientLocation.Text = "<no client selected>";
+            // 
+            // btnFindClient
+            // 
+            this.btnFindClient.Location = new System.Drawing.Point(12, 12);
+            this.btnFindClient.Name = "btnFindClient";
+            this.btnFindClient.Size = new System.Drawing.Size(138, 23);
+            this.btnFindClient.TabIndex = 1;
+            this.btnFindClient.Text = "Locate Client";
+            this.btnFindClient.UseVisualStyleBackColor = true;
+            this.btnFindClient.Click += new System.EventHandler(this.BtnFindClient_Click);
+            // 
+            // clientFolderDlg
+            // 
+            this.clientFolderDlg.Description = "Please locate the root of the game client where the game_pak file is located";
+            this.clientFolderDlg.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.clientFolderDlg.SelectedPath = "C:\\ArcheAge\\Working";
+            this.clientFolderDlg.ShowNewFolderButton = false;
+            // 
+            // btnQuestSphere
+            // 
+            this.btnQuestSphere.Enabled = false;
+            this.btnQuestSphere.Location = new System.Drawing.Point(12, 63);
+            this.btnQuestSphere.Name = "btnQuestSphere";
+            this.btnQuestSphere.Size = new System.Drawing.Size(138, 23);
+            this.btnQuestSphere.TabIndex = 2;
+            this.btnQuestSphere.Text = "Quest Sphere Data";
+            this.btnQuestSphere.UseVisualStyleBackColor = true;
+            this.btnQuestSphere.Click += new System.EventHandler(this.BtnQuestSphere_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Export";
+            // 
+            // exportFileDlg
+            // 
+            this.exportFileDlg.Filter = "json files|*.json";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnQuestSphere);
+            this.Controls.Add(this.btnFindClient);
+            this.Controls.Add(this.LClientLocation);
+            this.Name = "MainForm";
+            this.Text = "Client Data Exporter";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LClientLocation;
+        private System.Windows.Forms.Button btnFindClient;
+        private System.Windows.Forms.FolderBrowserDialog clientFolderDlg;
+        private System.Windows.Forms.Button btnQuestSphere;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog exportFileDlg;
+    }
+}
+
---
+++ AAEmu.ClientDataExporter/MainForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using AAPakEditor;
+
+namespace AAEmu.ClientDataExporter
+{
+    public partial class MainForm : Form
+    {
+        private class QuestSphereEntry
+        {
+            public int questID = -1 ;
+            public int componentID = -1;
+            public double X = 0.0f;
+            public double Y = 0.0f;
+            public double Z = 0.0f;
+            public double radius = 0.0f;
+        }
+
+        private AAPak pak = null ;
+        
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (pak != null)
+            {
+                pak.ClosePak();
+                pak = null;
+            }
+        }
+
+        private void BtnFindClient_Click(object sender, EventArgs e)
+        {
+            if (clientFolderDlg.ShowDialog() == DialogResult.OK)
+            {
+                Application.UseWaitCursor = true;
+                LClientLocation.Text = "<loading>";
+                LClientLocation.Update();
+                if (pak != null)
+                    pak.ClosePak();
+                pak = new AAPak(clientFolderDlg.SelectedPath + Path.DirectorySeparatorChar + "game_pak",true,false);
+                btnQuestSphere.Enabled = (pak.isOpen);
+                LClientLocation.Text = clientFolderDlg.SelectedPath;
+            }
+            Application.UseWaitCursor = false;
+        }
+
+        private void BtnQuestSphere_Click(object sender, EventArgs e)
+        {
+            if ((pak == null) || (!pak.isOpen))
+                return;
+
+            var qsd = new List<QuestSphereEntry>();
+            var sl = new List<string>();
+
+            // Find all related files and concat them into a giant stringlist
+            foreach(var pfi in pak.files)
+            {
+                if (pfi.name.ToLower().EndsWith("quest_sphere_data.g"))
+                {
+                    var thisStream = pak.ExportFileAsStream(pfi);
+                    using (var rs = new StreamReader(thisStream))
+                    {
+                        while(!rs.EndOfStream)
+                        {
+                            sl.Add(rs.ReadLine().Trim(' ').Trim('\t').ToLower());
+                        }
+                    }
+                }
+            }
+
+            for(int i = 0; i < sl.Count - 3;i++)
+            {
+                var l1 = sl[i + 0];
+                var l2 = sl[i + 1];
+                var l3 = sl[i + 2];
+                var l4 = sl[i + 3];
+                Add parser here
+
+            }
+
+
+        }
+    }
+}
